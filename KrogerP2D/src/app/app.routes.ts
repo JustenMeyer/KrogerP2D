87bf8e26a3d717053577a45c6c1654790bd7727f
JustenMeyer <justen.meyer862@gmail.com>
2025-07-22 22:06:23 -0400

    adding new components and routes

--- KrogerP2D/src/app/app.routes.ts
+++ KrogerP2D/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { About } from './features/about/about';
+import { ContactUs } from './features/contact-us/contact-us';
+import { Login } from './features/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'contact-us',
+    component: ContactUs
+  },
+  {
+    path: 'login',
+    component: Login
+  }
+];
